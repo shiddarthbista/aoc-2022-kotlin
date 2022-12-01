b12f0fc5d7fc3c8d136904911d6736f1bd11a15a
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2022-12-01 01:41:55 -0500

    day 1 aoc 2022

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
+import kotlin.time.measureTime
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+         return getCalorieSums(input).maxOrNull() ?: 0
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calorieSums = getCalorieSums(input)
+        calorieSums.sortDescending()
+        return calorieSums.subList(0,3).sum()
     }
+
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun getCalorieSums(input: List<String>): MutableList<Int> {
+    val sums = mutableListOf<Int>()
+    for (calories in input) {
+        if (calories.isEmpty()) {
+            sums += 0
+        } else {
+            var sum = sums.removeLastOrNull() ?: 0
+            sum += calories.toInt()
+            sums += sum
+        }
+    }
+    return sums
 }
