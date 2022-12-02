378acd4cb2ddbf2a48d8660e4aa128bdba475672
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2022-12-02 13:05:34 -0500

    refactor day 2 solution

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -3,55 +3,40 @@ package day02
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        var totalScore = 0
-        input.forEach {
-            totalScore += calculateScore(it.substringBefore(" "), it.substringAfter(" "))
+    fun part1(input: List<String>): Int =
+        input.sumOf {
+            calculateScore(it.substringBefore(" "), it.substringAfter(" "))
         }
-        return totalScore
-    }
 
-    fun part2(input: List<String>): Int {
-        var totalScore = 0
-        input.forEach {
-            totalScore += calculateScorePart2(it.substringBefore(" "), it.substringAfter(" "))
+    fun part2(input: List<String>): Int =
+        input.sumOf {
+            calculateScorePart2(it.substringBefore(" "), it.substringAfter(" "))
         }
-        return totalScore
-    }
 
     val testInput = readInput("Day02_test")
     val input = readInput("Day02")
     check(part1(testInput) == 15)
     check(part2(testInput) == 12)
 
-    println(part1(testInput))
-    println(part2(testInput))
     println(part1(input))
     println(part2(input))
 }
 
 fun calculateScore(opponent: String, player: String): Int {
-    val score: Int
-
     val pointBook = mapOf(
         "A" to mapOf("X" to 4, "Y" to 8, "Z" to 3),
         "B" to mapOf("X" to 1, "Y" to 5, "Z" to 9),
         "C" to mapOf("X" to 7, "Y" to 2, "Z" to 6)
     )
-
-    score = pointBook[opponent]?.get(player) ?: 0
-    return score
+    return pointBook.getValue(opponent).getValue(player)
 }
 
 fun calculateScorePart2(opponent: String, result: String): Int {
-    val score: Int
-
     val pointBook = mapOf(
-        "A" to mapOf("X" to 0+3, "Y" to 3+1, "Z" to 6+2),
-        "B" to mapOf("X" to 0+1, "Y" to 3+2, "Z" to 6+3),
-        "C" to mapOf("X" to 0+2, "Y" to 3+3, "Z" to 6+1)
+        "A" to mapOf("X" to 3, "Y" to 4, "Z" to 8),
+        "B" to mapOf("X" to 1, "Y" to 5, "Z" to 9),
+        "C" to mapOf("X" to 2, "Y" to 6, "Z" to 7)
     )
 
-    score = pointBook[opponent]?.get(result) ?: 0
-    return score
+    return pointBook.getValue(opponent).getValue(result)
 }
