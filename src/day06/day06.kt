ab3f2f9b7c539c7e417facf4e21ce3ea59005491
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2022-12-06 01:00:19 -0500

    day 06 solution refactor

--- src/day06/day06.kt
+++ src/day06/day06.kt
@@ -3,25 +3,15 @@ package day06
 import java.io.File
 
 fun main() {
+    fun String.findMarker(distinctCount: Int) = windowed(distinctCount).indexOfFirst{
+        it.toSet().count() == it.count()
+    } + distinctCount
 
-    fun part1(input: String):Int {
-        return input.windowed(4).indexOfFirst{
-            it.allUnique()
-        }
-    }
-
-    fun part2(input: String):Int {
-        return input.windowed(14).indexOfFirst{
-            it.allUnique()
-        }
-    }
+    fun part1(input: String):Int = input.findMarker(4)
+    fun part2(input: String):Int = input.findMarker(14)
 
     val testInput = File("src/inputs/Day06_test.txt").readText()
     val input = File("src/inputs/Day06.txt").readText()
-    println(testInput)
     println(part1(input))
-    println(part2(testInput))
     println(part2(input))
 }
-
-fun String.allUnique(): Boolean = all(hashSetOf<Char>()::add)
