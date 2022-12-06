34090f3872ffea032c1ec2cc7d4f6cea893e0628
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2022-12-06 01:00:39 -0500

    day 06 solution refactor

--- src/day06/day06.kt
+++ src/day06/day06.kt
@@ -10,7 +10,6 @@ fun main() {
     fun part1(input: String):Int = input.findMarker(4)
     fun part2(input: String):Int = input.findMarker(14)
 
-    val testInput = File("src/inputs/Day06_test.txt").readText()
     val input = File("src/inputs/Day06.txt").readText()
     println(part1(input))
     println(part2(input))
